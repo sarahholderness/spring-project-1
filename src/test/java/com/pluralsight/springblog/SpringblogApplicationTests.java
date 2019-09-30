83697bacafb6b55059bd6e07a3d84ff13b17d341
sarah <sarah-holderness@pluralsight.com>
2019-09-30 14:21:11 -0400

    First test

--- src/test/java/com/pluralsight/springblog/SpringblogApplicationTests.java
+++ src/test/java/com/pluralsight/springblog/SpringblogApplicationTests.java
@@ -2,15 +2,31 @@ package com.pluralsight.springblog;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SpringblogApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	public void contextLoads() {
+		try {
+			this.mvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string("Hello World"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
